26550515dad5a1ee8c9baed737831c3344e1f066
Hector-John <hectorjohn254@gmail.com>
2023-12-11 22:20:17 +0300

    navbar edit

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the text content of the spans
-    var originalH1 = document.getElementById('typedH1').textContent;
-    var originalP = document.getElementById('typedP').textContent;
+function typeText(elementId, originalText, index, speed) {
+    if (index < originalText.length) {
+        // Append the next character to the typedText span
+        document.getElementById(elementId).textContent += originalText.charAt(index);
 
-    // Clear the original text content
-    document.getElementById('typedH1').textContent = '';
-    document.getElementById('typedP').textContent = '';
-
-    // Function to simulate typing animation
-    function typeText(elementId, originalText, index, speed) {
-        if (index < originalText.length) {
-            // Append the next character to the typedText span
-            document.getElementById(elementId).textContent += originalText.charAt(index);
-            
-            // Call the function recursively with a customized delay
-            setTimeout(function () {
-                typeText(elementId, originalText, index + 1, speed);
-            }, speed); // Adjust the delay as needed
-        }
+        // Call the function recursively with a customized delay
+        setTimeout(function () {
+            typeText(elementId, originalText, index + 1, speed);
+        }, speed); // Adjust the delay as needed
     }
+}
 
-    // Call the typing animation function on page load for h1 and p
-    typeText('typedH1', originalH1, 0, 50); // Speed for h1
-    typeText('typedP', originalP, 0, 25); // Faster speed for p
-});
-
+typeText('typedH1', originalH1, 0, 50); 
+typeText('typedP', originalP, 0, 25); 
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+
+    const sidebarLinks = sidebar.querySelectorAll('a');
+    sidebarLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            window.location.href = link.href;
+
+            hideSidebar();
+        });
+    });
 }
 
 function hideSidebar() {
@@ -35,3 +31,7 @@ function hideSidebar() {
     sidebar.style.display = 'none';
 }
 
+function redirectToIndex() {
+    // window.location.href = 'blog.html';
+    window.open('blog.html', '_blank');
+}
