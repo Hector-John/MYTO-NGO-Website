daa2b40486b221ca82597c4332d1579360d8508d
Hector-John <hectorjohn254@gmail.com>
2023-12-19 13:07:52 +0300

    client changes implemented

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-function typeText(elementId, originalText, index, speed) {
+  // Define the typeText function
+  function typeText(elementId, originalText, index, speed) {
     if (index < originalText.length) {
-        // Append the next character to the typedText span
-        document.getElementById(elementId).textContent += originalText.charAt(index);
+      // Append the next character to the typedText span
+      document.getElementById(elementId).textContent += originalText.charAt(index);
 
-        // Call the function recursively with a customized delay
-        setTimeout(function () {
-            typeText(elementId, originalText, index + 1, speed);
-        }, speed); // Adjust the delay as needed
+      // Call the function recursively with a customized delay
+      setTimeout(function () {
+        typeText(elementId, originalText, index + 1, speed);
+      }, speed); // Adjust the delay as needed
     }
-}
+  }
 
-typeText('typedH1', originalH1, 0, 50); 
-typeText('typedP', originalP, 0, 25); 
+  // Define the original text for 'typedH1' and 'typedP'
+  var originalH1 = "Empowerment on Education, Economic and Health Care.";
+  var originalP = "To alleviate poverty, improve access to education, health care services and mitigating the impact of HIV/AIDs among orphans, vulnerable children and the poor in general.";
+
+  // Clear the initial content before starting the typing animation
+  document.getElementById('typedH1').textContent = '';
+  document.getElementById('typedP').textContent = '';
+
+  // Call the typeText function for the span with id "typedH1"
+  typeText('typedH1', originalH1, 0, 50);
+
+  // Call the typeText function for the span with id "typedP"
+  typeText('typedP', originalP, 0, 25);
+
+
+
+
+
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
